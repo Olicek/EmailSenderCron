5264c37c50568ca95fb94adaebdb38d93db3a9ab
Petr Olišar <petr.olisar@gmail.com>
2017-06-20 21:53:34 +0200

    Implement autoloader

--- bin/cron.php
+++ bin/cron.php
@@ -2,6 +2,18 @@
 <?php  declare(strict_types = 1);
 
 
+$autoloaderInWorkingDirectory = getcwd() . '/vendor/autoload.php';
+if (is_file($autoloaderInWorkingDirectory)) {
+  require_once($autoloaderInWorkingDirectory);
+}
+if (!class_exists('PHPStan\Command\AnalyseCommand', true)) {
+  $composerAutoloadFile = __DIR__ . '/../vendor/autoload.php';
+  if (!is_file($composerAutoloadFile)) {
+    $composerAutoloadFile = __DIR__ . '/../../../autoload.php';
+  }
+  require_once($composerAutoloadFile);
+}
+
 // Create the Application
 $application = new Symfony\Component\Console\Application();
 
